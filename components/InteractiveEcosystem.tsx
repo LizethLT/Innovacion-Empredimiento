1d152f53f8edc5a61978896b5741be49773c7e19
LizethLT <laimelizeth46@gmail.com>
2026-08-13 04:58:06 -0400

    Ultimas Modificaciones

--- components/InteractiveEcosystem.tsx
+++ components/InteractiveEcosystem.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
-import { Building2, Briefcase, Eye, GraduationCap, Handshake, Lightbulb, MapPin, Target, Users, X, Book, FileText, Gavel } from 'lucide-react'
+import { ArrowUpLeft, ArrowUpRight, Building2, Briefcase, Eye, GraduationCap, Handshake, Lightbulb, MapPin, Target, Users, X, Book, FileText, Gavel } from 'lucide-react'
 import { articles, getArticleByRange } from '@/lib/articles'
 
 const ITEMS = [
