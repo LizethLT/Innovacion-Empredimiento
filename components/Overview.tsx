c79214b244743c4d0209a22fa3b4f8132f863416
LizethLT <laimelizeth46@gmail.com>
2026-08-16 13:23:31 -0400

    Ultimas Modificaciones

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -269,17 +269,17 @@ const CONCEJAL_MODAL_HIGHLIGHT = {
 const SUPPORTERS = [
   { id: 'apoyo-1', name: 'Institución 1', logo: '/logos/oficina del concejal.png' },
   { id: 'apoyo-2', name: 'Institución 2', logo: '/logos/concejo municipal tarija.jpeg' },
-  { id: 'apoyo-3', name: 'Institución 3', logo: '/logos/esperanza bolivia.png' },
-  { id: 'apoyo-4', name: 'Institución 4', logo: '/logos/LOGO FAUTAPO.png' },
-  { id: 'apoyo-5', name: 'Institución 5', logo: '/logos/ECOSIES TARIJA.png' },
-  { id: 'apoyo-6', name: 'Institución 6', logo: '/logos/Logo Tarija Dialoga.png' },
-  { id: 'apoyo-7', name: 'Institución 7', logo: '/logos/aniv.png' },
-  { id: 'apoyo-8', name: 'Institución 8', logo: '/logos/federacion de empresarios.png' },
-  { id: 'apoyo-9', name: 'Institución 9', logo: '/logos/nativa.png' },
-  { id: 'apoyo-10', name: 'Institución 10', logo: '/logos/avina.png' },
-  { id: 'apoyo-10', name: 'Institución 11', logo: '/logos/comebol.png' },
-  { id: 'apoyo-10', name: 'Institución 12', logo: '/logos/UPDS.png' },
-  { id: 'apoyo-10', name: 'Institución 13', logo: '/logos/cropped-logo-UCB.png' },
+  { id: 'apoyo-3', name: 'Institución 3', logo: '/logos/LOGO FAUTAPO.png' },
+  { id: 'apoyo-4', name: 'Institución 4', logo: '/logos/ECOSIES TARIJA.png' },
+  { id: 'apoyo-5', name: 'Institución 5', logo: '/logos/Logo Tarija Dialoga.png' },
+  { id: 'apoyo-6', name: 'Institución 6', logo: '/logos/aniv.png' },
+  { id: 'apoyo-7', name: 'Institución 7', logo: '/logos/federacion de empresarios.png' },
+  { id: 'apoyo-8', name: 'Institución 8', logo: '/logos/UPDS.png' },
+  { id: 'apoyo-9', name: 'Institución 9', logo: '/logos/' },
+  { id: 'apoyo-10', name: 'Institución 10', logo: '/logos/' },
+  { id: 'apoyo-10', name: 'Institución 11', logo: '/logos/' },
+  { id: 'apoyo-10', name: 'Institución 12', logo: '/logos/' },
+  { id: 'apoyo-10', name: 'Institución 13', logo: '/logos/' },
 ]
 
 function renderOverviewArticleContent(content: string) {
